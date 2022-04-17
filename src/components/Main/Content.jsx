a09ae51209934e80bf980bc1604927bedbd0bc4f
askaraliev14 <61119063+askaraliev14@users.noreply.github.com>
2022-04-17 18:55:53 +0500

    Creating Content

---
+++ src/components/Main/Content.jsx
@@ -0,0 +1,14 @@
+import ContentCanvas from "./Particles";
+import {Box} from "@mui/material";
+
+const Content = () => {
+    return (
+        <ContentCanvas>
+            <Box>
+                
+            </Box>
+        </ContentCanvas>
+    )
+};
+
+export default Content;
