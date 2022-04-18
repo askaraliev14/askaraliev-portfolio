746f9cbeb91adaef69923b22bbcc301ce53e18e1
askaraliev14 <61119063+askaraliev14@users.noreply.github.com>
2022-04-18 08:58:07 +0500

    Creating Grid

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,31 +1,41 @@
 import Head from "next/head";
-import React, {Fragment, useCallback, useState} from "react";
+import React, {Fragment} from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import {ThemeProvider} from "@emotion/react";
-import {createTheme} from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import DrawerBar from "../Header/DrawerBar";
+import TopBar from "../Header/TopBar";
 
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#1976d2',
-        },
-    },
-});
 
 const AppLayout = ({children, title = "Askaraliev"}) => {
     return <Fragment>
-        <ThemeProvider theme={darkTheme}>
-            <Head>
-                <title>{title}</title>
-                <meta charSet="utf-8"/>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-            </Head>
-            <Header/>
-            {children}
-            <Footer/>
-        </ThemeProvider>
+            <Box
+                sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(4, 0.5fr)',
+                    gap: 1,
+                    gridTemplateRows: 'auto',
+                    gridTemplateAreas: `"header header header header"
+                                        "sidebar main main main"
+                                        "footer footer footer footer"`,
+                }}
+            >
+                <Box sx={{gridArea: 'header'}}><TopBar/></Box>
+                <Box sx={{gridArea: 'sidebar', width: "20px"}}><DrawerBar/></Box>
+                <Box sx={{gridArea: 'main'}}>{children}</Box>
+                <Box sx={{gridArea: 'footer'}}><Footer/></Box>
+            </Box>
+        {/*<Head>*/}
+        {/*    <title>{title}</title>*/}
+        {/*    <meta charSet="utf-8"/>*/}
+        {/*    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>*/}
+        {/*</Head>*/}
+
+        {/*<Header/>*/}
+
+        {/*{children}*/}
+
+        {/*<Footer/>*/}
     </Fragment>;
 };
 
