a59cf1aa603689e7982e9b6d35428161cff5fb1d
askaraliev14 <61119063+askaraliev14@users.noreply.github.com>
2022-04-18 08:53:37 +0500

    Header & Footer Modification

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,46 @@
-import {Box, Container} from '@mui/material';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 
-const Footer = () => {
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary">
+            {'Copyright © '}
+            <Link color="inherit" href="#">
+                Askaraliev
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+export default function Footer() {
     return (
         <footer>
-            <Box sx={{
-                borderTop: "1px solid #E7E7E7",
-                textAlign: "center",
-                padding: "20px",
-                position: "fixed",
-                left: "0",
-                bottom: "0",
-                zIndex: 1205,
-                height: "60px",
-                width: "100%",
-                background: "#ffb74d"}}>
-                <Container sx={{
-                    p: '1rem',
-                    color: 'white'
-                }}>
-                    <Box overflow="hidden">
-                        <h2>Footer</h2>
-                    </Box>
+            <Box
+                component="footer"
+                sx={{
+                    textAlign: "center",
+                    position: "fixed",
+                    left: "0",
+                    bottom: "0",
+                    zIndex: 1205,
+                    height: "60px",
+                    width: "100%",
+                    background: "#ffb74d"
+                }}
+            >
+                <Container maxWidth="sm">
+                    <Typography variant="body1">
+                    </Typography>
+                    <Copyright />
                 </Container>
             </Box>
         </footer>
-    )
-};
 
-export default Footer;
+    );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,110 +1,35 @@
 import * as React from 'react';
-import {styled, useTheme} from '@mui/material/styles';
-import MuiDrawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import {navLinks} from "../../data/data";
-import {useDispatch, useSelector} from "react-redux";
-import {navtogActions} from "../../store/navbar-togler";
 import TopBar from "./TopBar";
-import {drawerWidth} from "../../data/data";
 import {Fragment} from "react";
+import DrawerBar from "./DrawerBar";
+import {Container} from "@mui/material";
+import {createTheme} from "@mui/material/styles";
+import {ThemeProvider} from "@emotion/react";
 
-const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#1976d2',
+        },
     },
 });
 
-const DrawerHeader = styled('div')(({theme}) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-}));
-const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})(
-    ({theme, open}) => ({
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-        boxSizing: 'border-box',
-        ...(open && {
-            ...openedMixin(theme),
-            '& .MuiDrawer-paper': openedMixin(theme),
-        }),
-        ...(!open && {
-            ...closedMixin(theme),
-            '& .MuiDrawer-paper': closedMixin(theme),
-        }),
-    }),
-);
-
 const Header = () => {
-    const theme = useTheme();
-
-    const dispatch = useDispatch();
-    const open = useSelector(state => state.navtog.active);
-
-    const handleDrawerClose = () => {
-        dispatch(navtogActions.toggleActive(false))
-    };
     return (
         <Fragment>
-                <TopBar prop={drawerWidth}/>
-                <Drawer variant="permanent" open={open}>
-                    <DrawerHeader>
-                        <IconButton onClick={handleDrawerClose}>
-                            {theme.direction === 'rtl' ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
-                        </IconButton>
-                    </DrawerHeader>
-                    <Divider/>
-                    <List>
-                        {navLinks.map((link, index) => (
-                            <ListItemButton
-                                key={link}
-                                href={link.path}
-                                sx={{
-                                    minHeight: 48,
-                                    justifyContent: open ? 'initial' : 'center',
-                                    px: 2.5,
-                                }}
-                            >
-                                <ListItemIcon
-                                    sx={{
-                                        minWidth: 0,
-                                        mr: open ? 3 : 'auto',
-                                        justifyContent: 'center',
-                                    }}
-                                >
-                                    {link.icon}
-                                </ListItemIcon>
-                                <ListItemText primary={link.name} sx={{opacity: open ? 1 : 0}}/>
-                            </ListItemButton>
-                        ))}
-                    </List>
-                </Drawer>
+            <ThemeProvider theme={darkTheme}>
+                <Container sx={{
+                    position: "fixed",
+                    top: 0,
+                    width: "100%",
+                    display: "flex",
+                }}>
+
+                    <TopBar/>
+                    <DrawerBar/>
+                </Container>
+            </ThemeProvider>
         </Fragment>
     );
 }
